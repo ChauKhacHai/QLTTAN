93a091f7c77bd13be90533222caeceb8661bb4a2
ChauKhacHai <khachai.itcs@gmail.com>
2016-05-20 21:19:52 +0700

    Add KyThi

---
+++ DTO/LoaiDeThi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LoaiDeThi
+    {
+        string _maloaidethi;
+        string _tenloidethi;
+        float _thoiluongdethi;
+
+        public LoaiDeThi() { }
+        public LoaiDeThi(string maloaidethi, string tenloaidethi, float thoiluongthi)
+        {
+            this._maloaidethi = maloaidethi;
+            this._tenloidethi = tenloaidethi;
+            this._thoiluongdethi = thoiluongthi;
+        }
+
+    }
+}
---
+++ DTO/NgayHoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class NgayHoc
+    {
+        public string _mangay;
+        public DateTime _danhsachngay;
+        NgayHoc() { }
+        NgayHoc(string mangay,DateTime danhsachngay)
+        {
+
+        }
+    }
+}
