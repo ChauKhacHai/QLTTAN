aaa3895f1e7b09a74bb4d27b259aa038d352a9be
se13520769 <se13520769@gmail.com>
2016-05-20 16:24:16 +0700

    File DTO
    Viet DTO

---
+++ DTO/KhoaHoc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class KhoaHoc
+    {
+        public string _makhoahoc;
+        public string _tenkhoahoc;
+        public DateTime _ngaykhaigiang;
+        public KhoaHoc() { }
+        public KhoaHoc(string makhoahoc, string tenkhoahoc, DateTime ngaykhaigiang)
+        {
+            this._makhoahoc = makhoahoc;
+            this._tenkhoahoc = tenkhoahoc;
+            this._ngaykhaigiang = ngaykhaigiang;
+        }
+    }
+}
